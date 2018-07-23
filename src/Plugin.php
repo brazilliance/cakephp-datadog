9d6aac34b960cd5733854223f01d8231e426923f
Miquel Brazil <miquel@wone.co>
2018-07-23 16:55:33 -0400

    feat: add Plugin file

---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+namespace CakePHP_DataDog;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin class for CakePHP 3.6.0 plugin collection.
+ */
+class Plugin extends BasePlugin
+{
+    public function info()
+    {
+        echo 'Let\'s make sure this is working!';
+    }
+}
