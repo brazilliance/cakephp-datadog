548a364609c0c5b443610a6608cbeaa4af4f8eb1
Miquel Brazil <miquel@wone.co>
2018-07-23 18:34:33 -0400

    fix: remove GuzzleHttp to test autoloading

--- src/Services/DataDogService.php
+++ src/Services/DataDogService.php
@@ -1,13 +1,10 @@
 <?php
 namespace CakePHP_DataDog\Services;
 
-use GuzzleHttp;
-
 class DataDogService
 {
     public function info()
     {
-        $client = new GuzzleHttp();
         debug('I can reach in and get stuff from the DataDog service.');
         debug($client);
     }
